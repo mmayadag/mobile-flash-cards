6cb7f7f965ce9ceaf1d0f1ee275354196a5360d2
Murat Mayadağ <murat-2@hotmail.com>
2021-01-27 01:14:33 +0300

    ✨ feat: daily reminder with local notifications

--- App.js
+++ App.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+
+import { setLocalNotification } from './src/Utils/Notification';
+
 // Screens
 import HomeScreen from './src/Screens/Home';
 import AddCard from './src/Screens/AddCard';
@@ -10,30 +13,35 @@ import Quiz from './src/Screens/Quiz';
 
 const Stack = createStackNavigator();
 
-const App = () => (
-  <NavigationContainer>
-    <Stack.Navigator>
-      <Stack.Screen name="Decks" component={HomeScreen} />
-      <Stack.Screen name="Add Card" component={AddCard} />
-      <Stack.Screen
-        name="Deck"
-        component={Deck}
-        options={({ route }) => ({ title: route.params.title })}
-      />
-      <Stack.Screen
-        name="Cards"
-        component={Cards}
-        options={({ route }) => ({ title: route.params.title })}
-      />
-      <Stack.Screen
-        name="Quiz"
-        component={Quiz}
-        options={
-          ({ route }) => ({ title: route.params.title, headerBackTitle: 'Quiz' })
-        }
-      />
-    </Stack.Navigator>
-  </NavigationContainer>
-);
+const App = () => {
+  useEffect(() => {
+    setLocalNotification();
+  });
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Decks" component={HomeScreen} />
+        <Stack.Screen name="Add Card" component={AddCard} />
+        <Stack.Screen
+          name="Deck"
+          component={Deck}
+          options={({ route }) => ({ title: route.params.title })}
+        />
+        <Stack.Screen
+          name="Cards"
+          component={Cards}
+          options={({ route }) => ({ title: route.params.title })}
+        />
+        <Stack.Screen
+          name="Quiz"
+          component={Quiz}
+          options={
+            ({ route }) => ({ title: route.params.title, headerBackTitle: 'Quiz' })
+          }
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
 
 export default App;
